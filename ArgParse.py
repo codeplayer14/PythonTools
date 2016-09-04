a224fe5495520d69cc1dabcaf93bdd7d59be5939
iPaddy <padmanabh14@gmail.com>
2016-09-04 15:07:33 +0530

    ArgParse+output

--- ArgParse.py
+++ ArgParse.py
@@ -8,11 +8,18 @@ def fib(n):
 	
 def Main():
 	parser=argparse.ArgumentParser()
+	
 	parser.add_argument("num",help="The fibonacci number to be caluclated",type=int)
+	parser.add_argument("-o","--output",help= \
+	"Output result to a file",action="store_true")
 	
 	args=parser.parse_args()
 	result=fib(args.num)
 	print ("The " + str(args.num) + "th fib number is " + str(result))
+	
+	if(args.output):
+		f=open("fibonacci.txt","a")
+		f.write(str(result)+ "\n")
 	
 if __name__== '__main__':
 	Main()
